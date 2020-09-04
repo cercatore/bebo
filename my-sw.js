3ac13078f13eaa8cdf0367e2fb4157410f905035
cercatore <claudio.bagnato@gmail.com>
2020-09-04 15:07:24 +0200

    firebase step notification -2

--- my-sw.js
+++ my-sw.js
@@ -1,6 +1,3 @@
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
-
 
 
 firebase.initializeApp({
@@ -19,12 +16,13 @@ const messaging = firebase.messaging();
 messaging.setBackgroundMessageHandler(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
-
-  return self.registration.showNotification(notificationTitle,
-    notificationOptions);
+ 
 });
+const notificationTitle = 'Background Message Title';
+const notificationOptions = {
+  body: 'Background Message body.',
+  icon: '/firebase-logo.png'
+};
+
+return self.registration.showNotification(notificationTitle,
+  notificationOptions);
